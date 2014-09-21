f3c948f2f2dfdaa1235a2091272f923e7b857882
martinjhermida <martinhermida1@gmail.com>
2014-09-21 17:54:02 -0300

    Final version of cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,9 @@
-## Thes functions create a matrix and its inverse. The fist function
-## creates a matrix and formulas to access it and its inverse.
+## These functions create a matrix and its inverse. The fist function
+## creates a matrix and formulas to access it and its inverse from cache.
 ## The second function calculates and caches the inverse.
 
 ## makeCacheMatrix caches a matrix specified in the argument of the
-##functions. It also allows to call formulas to change the matrix or
+##function. It also allows to call formulas with $ to change the matrix or
 ## access it as well as its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
@@ -23,7 +23,7 @@ makeCacheMatrix <- function(x = matrix()) {
 
 ## cacheSolve calculates the inverse of a matrix and stores it in the 
 ## cache. If the inverse of the matrix was previously stored it accesses
-## the cache.
+## the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
